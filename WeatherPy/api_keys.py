7d1e1ec8b8afa03bd00b885f20e497d767fccb3f
markhirsch91 <markhirsch91@gmail.com>
2020-07-19 15:05:49 -0400

    Backing up homework

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cded9c418526700a23843e44ea3043d1"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC49alX4gZzcLmAK5QMm3Nsfby0h0OGfo4"
